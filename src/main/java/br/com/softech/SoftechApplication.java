35d0ce78e31c3916ac3d77215843629a5ea45f29
Robedson SAINTPHARD <pierreobed67@gmail.com>
2026-07-23 11:04:57 -0300

    feat: Adiciona classe principal SoftechApplication

---
+++ src/main/java/br/com/softech/SoftechApplication.java
@@ -0,0 +1,13 @@
+package br.com.softech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftechApplication.class, args);
+	}
+
+}
